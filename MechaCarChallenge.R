980e215ddabdae88f8981eb896cd05c19d0405a5
Britni Grimm <britnijoannegrimm@gmail.com>
2021-02-10 17:12:45 -0500

    Adding Summary Stats on Suspension Coils analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_table)) #generate summary statistics
 
+
+#Summary Statistics on Suspension Coils
+coils_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors =F) #read Suspension_Coil.csv as a table
+
+total_summary <- coils_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create a dataframe of PSI summary statistics
+
+lot_summary <- coils_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create a dataframe of PSI summary statistics grouped by lot
