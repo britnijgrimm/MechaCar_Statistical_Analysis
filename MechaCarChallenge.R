8543bffcfc36f6b86bd1113a682b2d3e3689d11c
Britni Grimm <britnijoannegrimm@gmail.com>
2021-02-11 11:25:17 -0500

    Adding Suspension Coil t-test Analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,11 @@ coils_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFact
 total_summary <- coils_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create a dataframe of PSI summary statistics
 
 lot_summary <- coils_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create a dataframe of PSI summary statistics grouped by lot
+
+
+#T-Tests on Suspension Coils
+t.test(coils_table$PSI, mu=mean(total_summary$Mean)) #perform a t-test to determine if the PSI across all manufacturing lots is statistically different from the population mean
+
+t.test(coils_table$PSI, mu=mean(lot_summary$Mean), subset=(lot_summary$Manufacturing_Lot=='Lot1')) #perform a t-test to determine if the PSI for Lot 1 is statistically different from the population mean
+t.test(coils_table$PSI, mu=mean(lot_summary$Mean), subset=(lot_summary$Manufacturing_Lot=='Lot2')) #perform a t-test to determine if the PSI for Lot 2 is statistically different from the population mean
+t.test(coils_table$PSI, mu=mean(lot_summary$Mean), subset=(lot_summary$Manufacturing_Lot=='Lot3')) #perform a t-test to determine if the PSI for Lot 3 is statistically different from the population mean
